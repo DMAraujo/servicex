dab8d1e1fc44a83151617b9492918d1b6bfebaa1
Diego Moura Araújo <116116522+DMAraujo@users.noreply.github.com>
2024-03-11 20:50:52 -0300

    Segundo Commit

--- src/main/java/br/com/diegoma/servicex/categoria/resources/CategoriaResource.java
+++ src/main/java/br/com/diegoma/servicex/categoria/resources/CategoriaResource.java
@@ -5,13 +5,12 @@ import br.com.diegoma.servicex.categoria.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categoria")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
     @Autowired
     private CategoriaService categoriaService;
@@ -19,6 +18,17 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria) {
         Categoria novaCategoria = categoriaService.criarCategoria(categoria);
         return new ResponseEntity<>(novaCategoria, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<Categoria>> listarCategorias() {
+        List<Categoria> categorias = categoriaService.listarCategoria();
+        return new ResponseEntity<>(categorias, HttpStatus.OK);
+    }
+    @GetMapping("/{idCategoria}")
+    public ResponseEntity<Categoria> buscarCategoria(@PathVariable Integer idCategoria) {
+         return categoriaService.buscarCategoriaPorId(idCategoria)
+                .map(categoria -> new ResponseEntity(categoria, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
     }
 
 }
