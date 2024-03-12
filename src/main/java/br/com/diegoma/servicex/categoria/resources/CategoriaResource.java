24394816b33cb033bf4ea66f7dc56c0cda26f047
Diego Moura Araújo <116116522+DMAraujo@users.noreply.github.com>
2024-03-11 21:26:01 -0300

    Terceiro Commit

--- src/main/java/br/com/diegoma/servicex/categoria/resources/CategoriaResource.java
+++ src/main/java/br/com/diegoma/servicex/categoria/resources/CategoriaResource.java
@@ -30,5 +30,20 @@ public class CategoriaResource {
                 .map(categoria -> new ResponseEntity(categoria, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
     }
-
+    @DeleteMapping("/{idCategoria}")
+    public ResponseEntity<Void> deletarCategoria(@PathVariable Integer idCategoria) {
+        categoriaService.deletarCategoria(idCategoria);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @PutMapping
+    public ResponseEntity<Categoria> atualizarCategoria(
+            @PathVariable Integer idCategoria,
+            @RequestBody Categoria categoria) {
+        if(!categoriaService.buscarCategoriaPorId(idCategoria).isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        categoria.setIdCategoria(idCategoria);
+        Categoria novaCategoria = categoriaService.atualizarCategoria(categoria);
+        return new ResponseEntity<>(novaCategoria, HttpStatus.OK);
+    }
 }
